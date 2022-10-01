67cce85cb43f9e3b2440a1b81b58e79a1d292064
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-10-01 23:51:05 +0600

    Simple React Counter App Done

--- react-simple-counter/src/components/Counter.js
+++ react-simple-counter/src/components/Counter.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Counter() {
+  const [count, setCount] = useState(0)
+  const increments = ()=>{
+    setCount(prevCount => prevCount+1)
+  }
+  const decrements = ()=>{
+    setCount(prevCount => prevCount-1)
+  }
+
   return (
     <>
         <div className="max-w-md mx-auto mt-10 space-y-5">
-            <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-slate-100 rounded shadow ">
-                <div className="text-2xl font-semibold" id="counter2">0</div>
+            <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-lime-100 rounded shadow ">
+                <div className="text-2xl font-semibold">{count}</div>
                 <div className="flex  space-x-3">
-                    <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment2">Increment</button>
-                    <button className="bg-purple-400 text-white px-3 py-2 rounded shadow" id="decrement2">Decrement</button>
-                </div>
+                    <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={increments}>Increment</button>
+                    <button className="bg-purple-400 text-white px-3 py-2 rounded shadow" onClick={decrements}>Decrement</button>
+                </div> 
             </div>
         </div>
     </>
--- react-simple-counter/src/components/Home.js
+++ react-simple-counter/src/components/Home.js
@@ -4,7 +4,7 @@ import Counter from './Counter'
 function Home() {
   return (
     <>
-        <div className='w-screen h-screen p-10 bg-gray-100 text-state-700'>
+        <div className='w-screen h-screen p-10 bg-stone-200 text-state-700'>
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">React Simple Counter Application</h1>
             <div className='max-w-md mx-auto mt-10 space-y-5'>
                 <Counter/>
