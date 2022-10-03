af0e70c1bc8b1ba271324c2d99f290c45e7e4e04
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-10-03 21:55:58 +0600

    Create a new react-app, copy the react simple counter codes and tailwindcss configure using npm

---
+++ react-total-counter/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Counter from './Counter'
+
+function Home() {
+  return (
+    <>
+        <div className='w-screen h-screen p-10 bg-stone-200 text-state-700'>
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">React Simple Counter Application</h1>
+            <div className='max-w-md mx-auto mt-10 space-y-5'>
+                <Counter/>
+                <Counter/>
+                <Counter/>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home;
