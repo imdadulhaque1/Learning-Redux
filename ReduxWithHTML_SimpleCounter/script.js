6abb2d3ed8c26dfb2a38b3318a4f3285627395fb
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-01-22 00:52:25 +0600

    Script customized via creating Action Identifier and Action Creator and also dynamicly payload's value send

--- ReduxWithHTML_SimpleCounter/script.js
+++ ReduxWithHTML_SimpleCounter/script.js
@@ -3,6 +3,24 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// Action Identifiers
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+// Action Creators
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+
 // Initilize the State
 const InitialState = {
   value: 0,
@@ -10,15 +28,15 @@ const InitialState = {
 
 // Create Reducer Function
 function counterReducer(state = InitialState, action) {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT) {
     return {
       ...state,
-      value: state.value + 1,
+      value: state.value + action.payload,
     };
-  } else if (action.type === "decrement") {
+  } else if (action.type === DECREMENT) {
     return {
       ...state,
-      value: state.value - 1,
+      value: state.value - action.payload,
     };
   } else {
     return state;
@@ -40,12 +58,8 @@ store.subscribe(render);
 
 // Button Click Listener
 incrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "increment",
-  });
+  store.dispatch(increment(5));
 });
 decrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "decrement",
-  });
+  store.dispatch(decrement(2));
 });
