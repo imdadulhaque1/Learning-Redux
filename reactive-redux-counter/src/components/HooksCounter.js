825b4e82e802e9426086fbd79d52b4634e2eee68
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-01-27 23:16:31 +0600

    use of useDispatch() function instead of mapDispatchToProps function to make it simplicity

--- reactive-redux-counter/src/components/HooksCounter.js
+++ reactive-redux-counter/src/components/HooksCounter.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { incrementFunc, decrementFunc } from "./redux/counter/actions";
 
-function HooksCounter({ dispatchIncrements, dispatchDecrements, id }) {
+function HooksCounter() {
   //!use of useSelector() and must used it top of the component and it works like state in react
   const countState = useSelector((usState) => usState.value);
-  //TODO: If I use the useSelector(), then don't need to use mapStateToProps function to get the value
+  const dispatch = useDispatch();
+  const dispatchIncrements = (incrementValue) => {
+    dispatch(incrementFunc(incrementValue));
+  };
+  const dispatchDecrements = (decrementValue) => {
+    dispatch(decrementFunc(decrementValue));
+  };
   return (
     <>
       <div className="max-w-md mx-auto mt-10 space-y-5">
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-slate-400 rounded shadow ">
-          <div className="text-2xl font-semibold">
-            {countState} {id}
-          </div>
+          <div className="text-2xl font-semibold">{countState}</div>
           <div className="flex  space-x-3">
             <button
               className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-              onClick={dispatchIncrements}
+              onClick={(incValue) => dispatchIncrements(incValue)}
             >
               Increment
             </button>
             <button
               className="bg-purple-400 text-white px-3 py-2 rounded shadow"
-              onClick={dispatchDecrements}
+              onClick={(decValue) => dispatchDecrements(decValue)}
             >
               Decrement
             </button>
@@ -33,17 +37,4 @@ function HooksCounter({ dispatchIncrements, dispatchDecrements, id }) {
   );
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     countState: state.value,
-//   };
-// };
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     dispatchIncrements: (value) => dispatch(incrementFunc(value)),
-//     dispatchDecrements: (value) => dispatch(decrementFunc(value)),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 export default HooksCounter;
