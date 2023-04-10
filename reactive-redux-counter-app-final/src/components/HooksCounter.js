755a44bbe3231e88d1adda46e06bfcba81da1e5d
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-04-10 21:19:00 +0600

    useDispatch() function implemented

--- reactive-redux-counter-app-final/src/components/HooksCounter.js
+++ reactive-redux-counter-app-final/src/components/HooksCounter.js
@@ -1,33 +1,40 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   decrementCreator,
   incrementCreator,
 } from "../redux/counter/actionCreator";
 
-const HooksCounter = ({ id }) => {
+const HooksCounter = () => {
   const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  const incrementValue = (value) => {
+    dispatch(incrementCreator(value));
+  };
+
+  const decrementValue = (value) => {
+    dispatch(decrementCreator(value));
+  };
   return (
     <>
       <div className="max-w-md mx-auto mt-10 space-y-5">
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-lime-100 rounded shadow ">
-          <div className="text-2xl font-semibold">
-            {count} -{id}
-          </div>
-          {/* <div className="flex  space-x-3">
+          <div className="text-2xl font-semibold">{count}</div>
+          <div className="flex  space-x-3">
             <button
               className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-              onClick={increments}
+              onClick={() => incrementValue(5)}
             >
               Increment
             </button>
             <button
               className="bg-purple-400 text-white px-3 py-2 rounded shadow"
-              onClick={decrements}
+              onClick={() => decrementValue(2)}
             >
               Decrement
             </button>
-          </div> */}
+          </div>
         </div>
       </div>
     </>
