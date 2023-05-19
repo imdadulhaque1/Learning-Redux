6df27409754d30806f0d50b29d6b4e41a4b10726
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-05-19 12:07:10 +0600

    redux and react-redux setup, created actionTypes and actionCreators

--- reactive-redux-counterapp-lastfinal/src/components/Home.js
+++ reactive-redux-counterapp-lastfinal/src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <>
       <div className="w-screen h-screen p-10 bg-stone-200 text-state-700">
         <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-          React Simple Counter Application
+          Reactive Redux Counter App
         </h1>
         <div className="max-w-md mx-auto mt-10 space-y-5">
           <Counter />
