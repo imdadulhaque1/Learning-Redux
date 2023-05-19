1c4bb6bf9bd1eda13f891a8950fcef14cca15f94
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-05-19 11:20:22 +0600

    Newly created reactive-redux-counterapp-lastfinal and setup with tailwindCSS and previous code

---
+++ reactive-redux-simple-todo-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Noto Sans HK", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
