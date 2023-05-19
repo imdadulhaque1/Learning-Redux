3c4b14a32e759b722dda506f47f62a192a6e2729
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-05-20 00:22:28 +0600

    Use of useDispatch()

--- reactive-redux-counterapp-lastfinal/src/components/HooksCounter.js
+++ reactive-redux-counterapp-lastfinal/src/components/HooksCounter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   decrementCreator,
   incrementCreator,
@@ -7,25 +7,33 @@ import {
 
 const HooksCounter = () => {
   const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+  const incrementHandler = (value) => {
+    dispatch(incrementCreator(value));
+  };
+
+  const decrementHandler = (value) => {
+    dispatch(decrementCreator(value));
+  };
   return (
     <>
       <div className="max-w-md mx-auto mt-10 space-y-5">
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-zinc-300 rounded shadow ">
           <div className="text-2xl font-semibold">{count}</div>
-          {/* <div className="flex  space-x-3">
+          <div className="flex  space-x-3">
             <button
               className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-              onClick={increments}
+              onClick={() => incrementHandler(5)}
             >
               Increment
             </button>
             <button
               className="bg-purple-400 text-white px-3 py-2 rounded shadow"
-              onClick={decrements}
+              onClick={() => decrementHandler(2)}
             >
               Decrement
             </button>
-          </div> */}
+          </div>
         </div>
       </div>
     </>
