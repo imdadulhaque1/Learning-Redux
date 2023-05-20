6b06a65963e485aa0bee9dfb89203d05dae44f08
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-05-20 13:41:53 +0600

    Customed Middleware concept

--- reactive-redux-counterapp-lastfinal/src/redux/store.js
+++ reactive-redux-counterapp-lastfinal/src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Current Action: ${JSON.stringify(action)}`);
+  console.log(`Before Action: ${JSON.stringify(store.getState())}`);
+
+  const upcommingState = [action].reduce(rootReducer, store.getState());
+  console.log(`Upcomming Action: ${JSON.stringify(upcommingState)}`);
+
+  //TODO: Passing Action
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
