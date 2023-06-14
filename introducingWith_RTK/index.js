a68aea3d477d3b9d9c261d7291db1ef6d480c343
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-06-14 17:51:12 +0600

    Dynamicly counter Features added

--- introducingWith_RTK/index.js
+++ introducingWith_RTK/index.js
@@ -1,15 +1,23 @@
 const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
+const {
+  dynamicCounterActions,
+} = require("./features/dynamicCounter/dynamicCounterSlice");
 
 // initial State
-console.log(store.getState());
+console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 
 // subscribe to state chnages
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-// dispatch actions
+// // dispatch actions
 store.dispatch(counterActions.increment());
 store.dispatch(counterActions.increment());
 store.dispatch(counterActions.decrement());
+
+// Dynamic action dispatch
+store.dispatch(dynamicCounterActions.dynamicIncrement(10));
+store.dispatch(dynamicCounterActions.dynamicIncrement(5));
+store.dispatch(dynamicCounterActions.dynamicDecrement(5));
